74fcecf63b8bb37041fc21ca53c54598ca779fcb
Eriq Augustine <eriq.augustine@gmail.com>
2018-01-04 15:54:38 -0800

    Augmenting the CI script for later use with psl-experimental.

--- .ci/build_core.sh
+++ .ci/build_core.sh
@@ -3,6 +3,7 @@
 # Build and install the PSL core matching this version.
 
 BUILD_DIR='/tmp/__building_psl_core__'
+TARGET_REPOS='psl'
 
 # If the pushed branch is master, we will use master for core.
 # Otherwise, we will use develop.
@@ -13,15 +14,16 @@ fi
 
 # We will always use the same owner for PSL core as this repo's owner.
 owner=$(echo "$TRAVIS_REPO_SLUG" | sed 's#/.\+$##')
-repo='psl'
 gitUrl="https://github.com/${owner}/${repo}.git"
 
-rm -Rf "${BUILD_DIR}"
-mkdir -p "${BUILD_DIR}"
-cd "${BUILD_DIR}"
+for repo in "${TARGET_REPOS}"; do
+   rm -Rf "${BUILD_DIR}"
+   mkdir -p "${BUILD_DIR}"
+   cd "${BUILD_DIR}"
 
-git clone "${gitUrl}"
-cd "${repo}"
-git checkout "${branch}"
+   git clone "${gitUrl}"
+   cd "${repo}"
+   git checkout "${branch}"
 
-mvn clean install -DskipTests
+   mvn clean install -DskipTests
+done

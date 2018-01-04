078dc538aef97a56ac780613d351f5b42d32b463
Eriq Augustine <eriq.augustine@gmail.com>
2018-01-04 15:39:49 -0800

    Changed the dependend psl core version to the current snapshot. Added a step to the Travis process to fetch and build the current PSL core.

---
+++ .ci/build_core.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Build and install the PSL core matching this version.
+
+BUILD_DIR='/tmp/__building_psl_core__'
+
+# If the pushed branch is master, we will use master for core.
+# Otherwise, we will use develop.
+branch='develop'
+if [ "$TRAVIS_BRANCH" = 'master' ]; then
+   branch='master'
+fi
+
+# We will always use the same owner for PSL core as this repo's owner.
+owner=$(echo "$TRAVIS_REPO_SLUG" | sed 's#/.\+$##')
+repo=$(echo "$TRAVIS_REPO_SLUG" | sed 's#^.\+/##')
+gitUrl="https://github.com/${owner}/${repo}.git"
+
+pushd .
+
+rm -Rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
+cd "${BUILD_DIR}"
+
+git clone "${gitUrl}"
+cd "${repo}"
+git checkout "${branch}"
+
+mvn clean install -DskipTests
+
+popd
